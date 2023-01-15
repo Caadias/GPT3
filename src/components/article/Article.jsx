5fa1b7b716c370a307ecf5276950013c1eb46c51
Caadias <115312299+Caadias@users.noreply.github.com>
2023-01-15 20:20:19 +0100

    15/01/23 - update for blog

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import './article.css';
 
-const article = () => {
+const Article = ({ imgUrl, date, title }) => {
   return (
-    <div>
-        article
+    <div className="gpt3__blog-container_article">
+      <div className="gpt3__blog-container_article-image">
+        <img src={imgUrl} alt="blog" />
+      </div>
+      
     </div>
   )
 }
 
-export default article
+export default Article
